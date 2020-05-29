2d5bc80413f12ea882ccb66debd351cf757e2cd4
Trishal <trishaldayananda2@gmail.com>
2020-05-29 18:37:00 +0530

    minor changes

--- atm-finder.c
+++ atm-finder.c
@@ -1,90 +1,84 @@
-#include<stdio.h> 
-void dijkstra(int G[5][5],int n,int sa);//where "sa" is selected area
+#include<stdio.h>
+#define INFINITY 9999
+#define MAX 10
  
-int main()
-{
-	int i,j,n,u;
-	int G[5][5]={
+void dijikstra(int G[MAX][MAX], int n, int startnode);
+ 
+void main(){
+	int G[MAX][MAX]={{0,2,5,8,6},{2,0,8,10,4},{5,8,0,8,6},{8,10,8,0,11},{6,4,6,11,0}};
+	int i, j, n=5, u;
+	printf("Enter your area\n1. Jayanagar\n2. Basavanagudi\n3. JP Nagar\n4. Bommanahalli\n5. Banashankari\n");
+	scanf("%d", &u);
+	switch(u)
+	{
+		case 1:
+		printf("The closest ATMs near you:\n");
+		printf("In Bommanahalli:\n");
+		printf("->1&, 2, Begur Main Rd\n");
+		printf("->65/2a, Devarachikkanahalli Main Rd\n");
+		break;
+		case 2:
+		printf("The closest ATMs near you:\n");
+		printf("In Basavanagudi");
+		printf("->198, Gandhi Bazar Main Rd, Gandhi Bazaar\n");
+		printf("")
 
-		{0,2,5,8,6},
-		{2,0,8,10,4},
-		{5,8,0,8,6},
-		{8,10,8,0,11},
-		{6,4,6,11,0}
-	};
-	
-	printf("\nChoose your Area:\n");
-	printf("1. Jayanagar\n2. Basavanagudi\n3. JP Nagar\n4. Bommanahalli\n5. Banashankari");
-	scanf("%d",&u);
-	dijkstra(G,n,u-1);
-	
-	return 0;
+
+	}
+	dijikstra(G,n,u-1);
+
 }
  
-void dijkstra(int G[5][5],int n,int sa)
+void dijikstra(int G[MAX][MAX], int n, int startnode)
 {
- 
-	int cost[5][5],distance[5],pred[5];
-	int visited[5],count,mindistance,nextnode,i,j;
-	
-	//pred[] stores the predecessor of each node
-	//count gives the number of nodes seen so far
-	//create the cost matrix
-	for(i=0;i<n;i++)
-		for(j=0;j<n;j++)
+	int cost[MAX][MAX], distance[MAX], pred[MAX];
+	int visited[MAX], count, mindistance, nextnode, i,j;
+	for(i=0;i < n;i++)
+		for(j=0;j < n;j++)
 			if(G[i][j]==0)
-				cost[i][j]=0;
+				cost[i][j]=INFINITY;
 			else
 				cost[i][j]=G[i][j];
 	
-	//initialize pred[],distance[] and visited[]
-	for(i=0;i<n;i++)
+	for(i=0;i< n;i++)
 	{
-		distance[i]=cost[sa][i];
-		pred[i]=sa;
+		distance[i]=cost[startnode][i];
+		pred[i]=startnode;
 		visited[i]=0;
 	}
-	
-	distance[sa]=0;
-	visited[sa]=1;
+	distance[startnode]=0;
+	visited[startnode]=1;
 	count=1;
-	
-	while(count<n-1)
-	{
-		mindistance=0;
-		
-		//nextnode gives the node at minimum distance
-		for(i=0;i<n;i++)
-			if(distance[i]<mindistance&&!visited[i])
+	while(count < n-1){
+		mindistance=INFINITY;
+		for(i=0;i < n;i++)
+			if(distance[i] < mindistance&&!visited[i])
 			{
 				mindistance=distance[i];
 				nextnode=i;
 			}
-			
-			//check if a better path exists through nextnode			
-			visited[nextnode]=1;
-			for(i=0;i<n;i++)
-				if(!visited[i])
-					if(mindistance+cost[nextnode][i]<distance[i])
-					{
-						distance[i]=mindistance+cost[nextnode][i];
-						pred[i]=nextnode;
-					}
-		count++;
+		visited[nextnode]=1;
+		for(i=0;i < n;i++)
+			if(!visited[i])
+				if(mindistance+cost[nextnode][i] < distance[i])
+				{
+					distance[i]=mindistance+cost[nextnode][i];
+					pred[i]=nextnode;
+				}
+			count++;
 	}
  
-	//print the path and distance of each node
-	for(i=0;i<n;i++)
-		if(i!=sa)
+	for(i=0;i < n;i++)
+		if(i!=startnode)
 		{
-			printf("\nDistance of node%d=%d",i,distance[i]);
-			printf("\nPath=%d",i);
-			
+			printf("\nDistance of %d = %d", i, distance[i]);
+			printf("\nPath = %d", i);
 			j=i;
 			do
 			{
 				j=pred[j];
-				printf("<-%d",j);
-			}while(j!=sa);
-	}
+				printf(" <-%d", j);
+			}
+			while(j!=startnode);
+		}
 }
